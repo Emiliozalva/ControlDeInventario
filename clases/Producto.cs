9486196c99c3ee8948ff50927e80cbbd31332a45
Emilio Zalva <emizalva08@gmail.com>
2025-10-21 11:26:36 -0300

    ...

--- clases/Producto.cs
+++ clases/Producto.cs
@@ -6,80 +6,25 @@ using System.Threading.Tasks;
 
 namespace SistemaDeInventarioASOEM.clases
 {
-    class Producto
+    public class Producto
     {
-        private int _id;
-        private string _nombre;
-        private string _modelo;
-        private string _marca;
-        private int _cantStock;
-        private int _cantPrestada;
-        Producto(int id, int cant, string nombre, string modelo, string marca)
-        {
-            if (cant < 0) { cant = cant * (-1); }
-            _id = id;
-            _nombre = nombre;
-            _modelo = modelo;
-            _marca = marca;
-            _cantStock = cant;
-            _cantPrestada = 0;
-        }
-        Producto(int id, int cant, int cantstock, string nombre, string modelo, string marca)
-        {
-            if (cant < 0) { cant = cant * (-1); }
-            _id = id;
-            _nombre = nombre;
-            _modelo = modelo;
-            _marca = marca;
-            _cantStock = cant;
-            _cantPrestada = cantstock;
-        }
-        Producto(int id, string nombre)
-        {
-            _id = id;
-            _nombre = nombre;
-            _modelo = "";
-            _marca = "";
-            _cantStock = 1;
-            _cantPrestada = 0;
-        }
-        Producto(int id, string nombre,int cant)
-        {   
-            if(cant < 0) { cant = cant * (-1); }
-            _id = id;
-            _nombre = nombre;
-            _modelo = "";
-            _marca = "";
-            _cantStock = cant;
-            _cantPrestada = 0;
-        }
-        void modificarNombre(string nombre) { _nombre = nombre; }
-        void modificarMarca(string marca) { _marca = marca; }
-        void modificarModelo(string modelo) {  _modelo = modelo; }
-        void modificarStock(int n) { _cantStock = n;}
-        int getId() {  return _id; }
-        string getMarca() { return _marca; }
-        string getModel() { return _modelo; }
-        string getNombre() { return _nombre; }
-        int getCantStock() { return _cantStock; }
-        void agregar_1_Producto() { _cantStock++; }
-        void eliminar_1_Producto() { 
-            if(!(_cantStock < 1)) { _cantStock--; }
-        }
-        void agregarProductos(int n) {
-            if (n < 0) { n = n * (-1); }
-            _cantStock = _cantStock + n; }
+        // La Columna se llama "IDproducto" y es la Clave Primaria
+        public int IDproducto { get; set; }
 
-        void eliminarProductos(int n)
-        {
-            if (n < 0){ n = n * (-1);}
-            _cantStock = _cantStock - n;
-            if(_cantStock < 0) { _cantStock = 0; }
-        }
-        void agregarPrestamo(/* Definir luegos los parametros */) { _cantPrestada++; /* Cambiar esto despues, agregar logica */ }
-        void eliminarPrestamo( /* Definir luegos los parametros */) { _cantPrestada--; /*Cambiar esto despues, agregar logica */ }
-        void agregarCantidadPrestada() { }
-        void eliminarCantidadPrestada() { } 
-        
+        // La Columna se llama "producto"
+        public string producto { get; set; }
+
+        // La Columna se llama "cantidadStock"
+        public int cantidadStock { get; set; }
+
+        // La Columna se llama "marca"
+        public string marca { get; set; }
+
+        // La Columna se llama "modelo"
+        public string modelo { get; set; }
+
+        // La Columna se llama "cantidadPrestada"
+        public int cantidadPrestada { get; set; }
     }
 }
+
